0f35193c58181dddf80c4a300bb1ab108009e158
Dhruv Pratap Singh <115564211+iamdpsingh@users.noreply.github.com>
2025-06-25 09:32:12 +0530

    Delete backend directory

--- backend/asgi.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f681fdccaf3e4979522c86ce562035388783f5b852d276ab1c580012a41272f1
-size 391
--- backend/wsgi.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:599fa4a228f8c5830b1b5d205fed2219309b984c7f6711baa195a8f4c5eb7a62
-size 391
