53e355de10ff93352d7353f378b6daa84c0dd9ed
Dhruv Pratap Singh <115564211+iamdpsingh@users.noreply.github.com>
2025-06-25 09:32:31 +0530

    Delete venv directory

--- venv/bin/activate.fish
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:35652ddf7d34451ad2c508109daeaeed80b83d731d7927e8c99e812940aa76b4
-size 2238
--- venv/lib/python3.13/site-packages/django/contrib/admin/static/admin/js/admin/RelatedObjectLookups.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b7cb5151e34a530454859be8c0cb35f9eb73a986bd75057200975c0234f63aeb
-size 9777
